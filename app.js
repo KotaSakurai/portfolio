5ae172f04421b694edeeead02493ca63fc2c3e06
diol <diolll25@gmail.com>
2016-04-29 23:47:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+
+
+//-----------------------------------------
+app.get('/', function(req, res){
+  fs.readFile('./index.html', 'utf-8', function(err, data){
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found');
+      return res.end();
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  })
+});
+
+app.listen(3000);
+
+
+
+//-----------------------------------------
+
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+
+module.exports = app;
