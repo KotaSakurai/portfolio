b5779abaa4b1eac01e33ab960201f13bb6deebaa
diol <diolll25@gmail.com>
2016-05-20 13:57:57 +0900

    new page conversation

--- top.js
+++ top.js
@@ -96,42 +96,44 @@ $(function() {
 		var j=1;
 
 		$("#profile1").toggle();
-		$("#profile1").delay(500).animate({
+		$("#profile1").delay(200).animate({
     	    	"opacity":"0.7",
     	    	"top": "70px",
     	    	"left": "125px"
     	});
 
+        /*
     	$("#profile6").toggle();
 		$("#profile6").delay(500).animate({
     	    	"opacity":"0.7",
     	    	"top": array[11]+"px",
     	    	"left": array[11+1] + "px"
     	});
+        */
 
     	$("#profile2").toggle();
-		$("#profile2").delay(1000).animate({
+		$("#profile2").delay(400).animate({
     	    	"opacity":"0.7",
     	    	"top": array[3]+"px",
     	    	"left": array[3+1] + "px"
     	});
 
     	$("#profile5").toggle();
-		$("#profile5").delay(1000).animate({
+		$("#profile5").delay(400).animate({
     	    	"opacity":"0.7",
     	    	"top": array[9]+"px",
     	    	"left": array[9+1] + "px"
     	});
 
     	$("#profile3").toggle();
-		$("#profile3").delay(1500).animate({
+		$("#profile3").delay(600).animate({
     	    	"opacity":"0.7",
     	    	"top": array[5]+"px",
     	    	"left": array[5+1] + "px"
     	});
 
     	$("#profile4").toggle();
-		$("#profile4").delay(1500).animate({
+		$("#profile4").delay(600).animate({
     	    	"opacity":"0.7",
     	    	"top": array[7]+"px",
     	    	"left": array[7+1] + "px"
@@ -159,9 +161,41 @@ $(function() {
         var array =[0,70,125,  200,50,  330,125, 70,275,  200,350, 330,275,   70,425,   200,500, 200,500];
         var j=1;
 
-        for(i=1; i < 9; i++){
+        for(i=1; i < 8; i++){
             $("#skill"+ i ).toggle();
             $("#skill" + i).delay(i * 100).animate({
+                "opacity":"0.8",
+                "top": array[j]+"px",
+                "left": array[j+1]+"px"
+            });
+            j += 2;
+        }
+
+    }
+
+    function circle_display_contact ( number ){
+        var array =[0,70,125,  200,50,  330,125, 70,275,  200,350, 330,275,   70,425,   200,500, 200,500];
+        var j=1;
+
+        for(i=1; i < 7; i++){
+            $("#contact"+ i ).toggle();
+            $("#contact" + i).delay(i * 150).animate({
+                "opacity":"0.8",
+                "top": array[j]+"px",
+                "left": array[j+1]+"px"
+            });
+            j += 2;
+        }
+
+    }
+
+    function circle_display_o_product ( number ){
+        var array =[0,70,125,  200,50,  330,125, 70,275,  200,350, 330,275,   70,425,   200,500, 200,500];
+        var j=1;
+
+        for(i=1; i < 3; i++){
+            $("#o_product"+ i ).toggle();
+            $("#o_product" + i).delay(i * 50).animate({
                 "opacity":"0.8",
                 "top": array[j]+"px",
                 "left": array[j+1]+"px"
@@ -196,6 +230,12 @@ $(function() {
         else if (display_what=="skill"){
             for(i=1; i<15; i++){
                 $("#skill"+i).hide();
+            }
+
+        }
+        else if (display_what=="o_product"){
+            for(i=1; i<15; i++){
+                $("#linestamp"+i).hide();
             }
 
         }
@@ -252,7 +292,17 @@ $(function() {
     });
 
     $("#position5").click(function(){
-    	
+    	hold_position = "#position5";
+        position_number = 5;
+        display_what = "o_product";             //表示のアニメ
+        
+        center_move("#position5");
+        toggle(5);
+
+        $("#cover").toggle();
+
+        circle_display_o_product();
+        display_backbutton();
     });
 
     $("#position6").click(function(){
@@ -270,24 +320,17 @@ $(function() {
     });
 
     $("#position7").click(function(){
-    	if ( flag == 0){
-    		flag = 1;
-    		center_move("#position7");
-    		toggle(7);
-      
-      		//表示のアニメ
-      		circle_display_profile();
-    	}
-    	else {
-    		flag = 0;
-    		//戻るアニメ
-			$("#position7").animate({
-            	"top":"330px",
-            	"left":"275px"
-        	});
-        	toggle(7); 
-        	circle_delete("profile");
-    	}  
+    	hold_position = "#position7";
+        position_number = 7;
+        display_what = "contact";             //表示のアニメ
+        
+        center_move("#position7");
+        toggle(7);
+
+        $("#cover").toggle();
+
+        circle_display_contact();
+        display_backbutton();
     });
 
 
@@ -298,11 +341,11 @@ $(function() {
             $(".description").toggle();
             $(".description2").toggle();
             $(".pic").toggle();
-            $("#sen1").html('<p>TODOリストにアイデアを足したTODOリストアプリ。TODOの入力が全てキューのように管理され、入力したTODOのうち、一番最初にいれたものしか見えないといったもの。そのことによってやり残すTODOがなくなるといったアプリ。</p>');
+            $("#sen1").html('<p>やり残してずっと残ってしまっているtodo、ありませんか？入力したToDoはキューのように管理され、次にデキューされるToDoしか見れないようになっています。そのことによってやるべきことは見えているToDoたった一つ、それを達成していくことで全てのToDoを完了していくことが可能です。</p>');
             $('.consept').html('<span >--- concept ---</span><br><span >やりのこしのないToDo app</span>');
             $(".title").html("<span>PAKUPAKUTODO</span>");
-            $("#sen3").html("<span >Lang:  HTML / CSS / JavaScript</span><br><span>Tool: Monaca(ハイブリッドアプリplatform)</span><br><span>Days: 4days</span><br><span >make : character / idea / logo design / layout / UI design </span><br><span> 2014/10</span><br>");
-            $(".pic").html('<img src="../image/pkpksc.png" width="375" height="667">');
+            $("#sen3").html('<table><tr><td>Lang :</td> <td class = "td1" >HTML / CSS / JavaScript</td> </tr> <tr> <td class="td1" >Tool :　</td> <td>Monaca(Hybrid Platform)</td> </tr> <tr> <td class="td1">Days :　</td> <td>4days</</td> </tr> <tr> <td class="td1">Make :　</td> <td>character / idea / logo design / layout / UI design</</td> </tr> <tr> <td class="td1">Team Member :　</td> <td>2</</td> </tr> <tr> <td class="td1">Date :　</td> <td>2014/10</</td> </tr> </table>');
+            $(".pic").html('<img src="./image/pkpksc.PNG" width="375" height="667">');
         },
         function(){
             $(".description").toggle();
@@ -318,11 +361,11 @@ $(function() {
             $(".description2").toggle();
             $(".pic").toggle();
 
-            $("#sen1").html('<p>アプリを使う仲間うちでそれぞれが好きなアーティストを登録し、その登録に応じたアーティストの曲をランダムで流す。その流れる音楽をそれぞれの端末でリアルタイムに同期され曲が流れる。そのことによって同時刻に同じ場所にいなくても同じ音楽を共有できるといったゆる〜いつながりを生み出す。</p>');
+            $("#sen1").html('<p>友人同士でグループを作成しそれぞれが好きなアーティストを登録、その登録に応じたアーティストの曲をランダムで流します。その流れる音楽をそれぞれの端末でリアルタイムに同期されラジオのように曲が流れることで、"音楽"を通したゆるいつながりを生みだします。</p>');
             $(".title").html("<span>RadioS</span>");
             $(".consept").html('<span >--- concept ---</span><br><span>音楽を共有してゆる〜いつながりを生みたい</span>');
-            $("#sen3").html('<span >Lang:  HTML / CSS / JavaScript</span><br><span>Tool: Monaca(ハイブリッドアプリplatform)</span><br><span>Days: 15days</span><br><span >make : character / idea / logo design / layout / UI design </span><br><span> 2015/11</span><br>');
-            $(".pic").html('<img src="../image/radiosc.png" width="375" height="667">');
+            $("#sen3").html('<table><tr><td>Lang :</td> <td class="td1">HTML / CSS / JavaScript</td> </tr> <tr> <td class="td1" >Tool :　</td> <td>Monaca(Hybrid Platform)</td> </tr> <tr> <td class="td1">Days :　</td> <td>14days</</td> </tr> <tr> <td class="td1">Make :　</td> <td>character / logo design / layout / UI design</</td> </tr> <tr> <td class="td1">Team Member :　</td> <td>3</</td> </tr> <tr> <td class="td1">Date :　</td> <td>2015/11</</td> </tr> </table>');
+            $(".pic").html('<img src="./image/radiosc.PNG" width="375" height="667">');
         },
         function(){
             $(".description").toggle();
@@ -339,11 +382,11 @@ $(function() {
             $(".description2").toggle();
             $(".pic").toggle();
 
-            $("#sen1").html('<p>"COCO-YAN" はTwitterから友達が行きたかった場所を一緒に探してくれます.</p><p>https://cocoyan.herokuapp.com/</p>');
+            $("#sen1").html('<p>"COCO-YAN"は、「実はここ、行きたかったんでしょ？」を提供できるWebアプリケーションです。特定ユーザーのつぶやきの解析を行い、その人が行きたいと思っていた場所を表示します。それを見て提案することによって喜んでもらえます。<br>https://cocoyan.herokuapp.com/</p>');
             $(".title").html("<span>COCO-YAN</span>");
             $(".consept").html('<span >--- concept ---</span><br><span>彼女にここいきたかったんでしょ？を提供して喜ばせたい</span>');
-            $("#sen3").html('<span >Lang:  HTML / CSS / JavaScript / Ruby / Ruby on rails </span><br><span>Tool: </span><br><span>Days: 13days</span><br><span >make : layout / idea / logo design / UI design </span><br><span> 2015/10</span><br>');
-            $(".pic").html('<img src="../image/cocoyansc.png" width="375" height="667">');
+            $("#sen3").html('<table><tr><td>Lang :</td> <td class="td1">HTML / CSS / JavaScript / Ruby </td> </tr>  <tr> <td class="td1">Days :　</td> <td>13days</</td> </tr> <tr> <td class="td1">Make :　</td> <td> UI design / logo design / layout / idea </td> </tr> <tr> <td class="td1">Team Member :　</td> <td>3</</td> </tr> <tr> <td class="td1">Date :　</td> <td>2015/10</</td> </tr> </table>');
+            $(".pic").html('<img src="./image/cocoyansc.PNG" width="375" height="667">');
         },
         function(){
             $(".description").toggle();
@@ -360,11 +403,11 @@ $(function() {
             $(".description2").toggle();
             $(".pic").toggle();
 
-            $("#sen1").html('<p>動くんです。</p><p>hack day 2016 by yahoo!　出場作品。磁石と砂鉄を組み合わせた時のかわいさで面白いものをつくれないかと工作した。</p>');
+            $("#sen1").html('<p>砂鉄と磁石を組み合わせたら何か面白いものがつくれるんじゃないのか？という発想のもと作成した作品。チーム:"琵琶湖から砂鉄もってきました"としてHackday 2016 by yahoo! にID:80で出場させていただきました。 youtube:<br> https://www.youtube.com/watch?v=lsujyuPHnxE</p>');
             $(".title").html("<span>動くんです。</span>");
             $(".consept").html('<span >--- concept ---</span><br><span>砂鉄×磁石×プログラミング</span>');
-            $("#sen3").html('<span >Lang: </span><br><span>Tool: Arduino / 3Dprinter </span><br><span>Days: 24 hours</span><br><span >make : idea / name / 組み立て / program </span><br><span> 2016/02</span><br>');
-            $(".pic").html('<img src="../image/hackdaysc.png" width="375" height="667">');
+            $("#sen3").html('<table> <tr> <td class="td1" >Tool :　</td> <td>Arduino / 3Dprinter</td> </tr> <tr> <td class="td1">Days :　</td> <td>24 hours</</td> </tr> <tr> <td class="td1">Make :　</td> <td> idea / 組み立て / program </td> </tr> <tr> <td class="td1">Team Member :　</td> <td>3</</td> </tr> <tr> <td class="td1">Date :　</td> <td>2016 / 2</</td> </tr> </table>');
+            $(".pic").html('<img src="./image/hackdaysc.PNG" width="375" height="667">');
         },
         function(){
             $(".description").toggle();
@@ -417,6 +460,17 @@ $(function() {
         }
     );
 
+     $("#profile5").hover(
+        function(){
+            $("#name10").toggle();
+            $("#name11").toggle();
+            $("#name12").toggle();
+            $("#name13").toggle();
+        },
+        function(){   
+        }
+    );
+
     $("#d1").hover(
         function(){
             $("#d_dis_product1").toggle();
@@ -464,6 +518,25 @@ $(function() {
         },
         function(){
             $("#d_dis_product6").toggle();
+        }
+    );
+
+    $("#o_product1").hover(
+        function(){
+            //のったときのほかのようそが全面にある場合があるのでけす
+            $("#linestamp2").hide();
+            $("#linestamp1").toggle();
+        },
+        function(){
+            
+        }
+    );
+    $("#o_product2").hover(
+        function(){
+            $("#linestamp2").toggle();
+        },
+        function(){
+            
         }
     );
 
