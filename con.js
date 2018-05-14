d6fffc210c72851a84993ab57e179a23f61cc646
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-05-15 01:20:07 +0900

    All commit

--- con.js
+++ con.js
@@ -1,1 +1,15 @@
-js
+$(function(){
+	var nav_offset = $('.head2').offset().top;
+
+	$(window).on('scroll load',function(){
+	  console.log(nav_offset);
+	  var now_offset = $(window).scrollTop();
+	  if ( now_offset >= nav_offset ) {
+	    $('.head2').css('position','fixed');
+	    
+	  } else {
+	    $('.head2').css('position','inherit');
+	  }
+	});
+});
+
